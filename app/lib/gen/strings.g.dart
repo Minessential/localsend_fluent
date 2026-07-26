7de690ba4e4c9028a2e9e15b49324e2235e4b91f
Tien Do Nam <dev.tien.donam@gmail.com>
2026-07-26 18:17:45 +0200

    i18n: drop quick save from favorites warning

--- app/lib/gen/strings.g.dart
+++ app/lib/gen/strings.g.dart
@@ -4,7 +4,7 @@
 /// To regenerate, run: `dart run slang`
 ///
 /// Locales: 55
-/// Strings: 18483 (336 per locale)
+/// Strings: 18393 (334 per locale)
 
 // coverage:ignore-file
 // ignore_for_file: type=lint, unused_import
