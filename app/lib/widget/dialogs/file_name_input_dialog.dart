f93531562d189f52084384e39bfdc0de1774a31e
Tien Do Nam <dev.tien.donam@gmail.com>
2026-08-01 17:17:41 +0200

    feat: path sanitizing in core

--- app/lib/widget/dialogs/file_name_input_dialog.dart
+++ app/lib/widget/dialogs/file_name_input_dialog.dart
@@ -1,9 +1,7 @@
-import 'dart:io';
-
 import 'package:fluent_ui/fluent_ui.dart';
-import 'package:legalize/legalize.dart';
 import 'package:localsend_app/gen/strings.g.dart';
 import 'package:localsend_app/widget/fluent/custom_text_box.dart';
+import 'package:localsend_isolates/rust/api/filename.dart';
 import 'package:localsend_isolates/util/file_path_helper.dart';
 import 'package:routerino/routerino.dart';
 
@@ -38,7 +36,7 @@ class _FileNameInputDialogState extends State<FileNameInputDialog> {
       return false;
     }
 
-    if (!isValidFilename(input, os: Platform.operatingSystem)) {
+    if (!isValidFileName(name: input)) {
       setState(() {
         _errorMessage = t.sanitization.invalid;
       });
