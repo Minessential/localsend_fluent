4321f2775bb720aecbf2b707ec11139f0c24adc2
Tien Do Nam <dev.tien.donam@gmail.com>
2026-08-04 13:12:03 +0200

    fix: correctly initialize selectedReceivingFilesProvider

--- app/lib/provider/network/server/controller/receive_controller.dart
+++ app/lib/provider/network/server/controller/receive_controller.dart
@@ -222,6 +222,8 @@ class ReceiveController {
       );
     });
 
+    server.ref.notifier(selectedReceivingFilesProvider).setFiles(files.values.toList());
+
     // ignore: use_build_context_synchronously, unawaited_futures
     Routerino.context.push(() => ReceivePage(receiveProvider));
   }
