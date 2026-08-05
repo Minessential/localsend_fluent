c1f89ef5327cf45fb5e6f07f80fe48a0d36e8f2f
Tien Do Nam <dev.tien.donam@gmail.com>
2026-08-05 13:04:26 +0200

    feat: make buttons in device details bigger

--- app/lib/widget/big_button.dart
+++ app/lib/widget/big_button.dart
@@ -8,19 +8,21 @@ class BigButton extends StatelessWidget {
   final IconData icon;
   final String label;
   final bool filled;
+  final double? width;
   final VoidCallback onTap;
 
   const BigButton({
     required this.icon,
     required this.label,
     required this.filled,
+    this.width,
     required this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
     final sizingInformation = SizingInformation(MediaQuery.sizeOf(context).width);
-    final buttonWidth = sizingInformation.isDesktop ? desktopWidth : mobileWidth;
+    final buttonWidth = width ?? (sizingInformation.isDesktop ? desktopWidth : mobileWidth);
     return SizedBox(
       width: buttonWidth,
       height: 68.0,
