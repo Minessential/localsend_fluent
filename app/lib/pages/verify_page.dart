localsend/localsend

feat: device detail

app/lib/pages/verify_page.dart
@@ -66,6 +66,14 @@ class _VerifyPageState extends State<VerifyPage> {
             ),
             _VerifyMode.raw => SelectableText(combined),
           },
+          const SizedBox(height: 20),
+          Center(
+            child: Text(
+              t.verifyPage.question,
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.bodyMedium?.copyWith(color: Theme.of(context).colorScheme.onSurfaceVariant),
+            ),
+          ),
         ],
       ),
     );

Minessential/localsend_fluent

app/lib/pages/verify_page.dart
@@ -89,6 +89,14 @@ class _VerifyPageState extends State<VerifyPage> {
               ),
             ),
           },
+          const SizedBox(height: 20),
+          Center(
+            child: Text(
+              t.verifyPage.question,
+              textAlign: TextAlign.center,
+              style: FluentTheme.of(context).typography.bodyStrong?.copyWith(color: FluentTheme.of(context).resources.textFillColorSecondary),
+            ),
+          ),
         ],
       ),
     );