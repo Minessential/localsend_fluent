localsend/localsend

feat: add verify button to send and receive page

app/lib/pages/device_details_page.dart
@@ -88,7 +88,11 @@ class _DeviceDetailsPageState extends State<DeviceDetailsPage> with Refena {
                 label: t.deviceDetailsPage.verify,
                 filled: false,
                 width: 120,
-                onTap: () async => await context.push(() => VerifyPage(device: device)),
+                onTap: () async => await context.push(
+                  () => VerifyPage(
+                    fingerprint: CombinedFingerprint.load(context, device.fingerprint),
+                  ),
+                ),
               ),
             ],
           ),

Minessential/localsend_fluent

app/lib/pages/device_details_page.dart
@@ -94,7 +94,11 @@ class _DeviceDetailsPageState extends State<DeviceDetailsPage> with Refena {
                 ButtonType.outlined,
                 icon: const Icon(FluentIcons.shield_checkmark_20_regular, size: 18),
                 label: Text(t.deviceDetailsPage.verify),
-                onPressed: () async => await context.push(() => VerifyPage(device: device)),
+                onPressed: () async => await context.push(
+                  () => VerifyPage(
+                    fingerprint: CombinedFingerprint.load(context, device.fingerprint),
+                  ),
+                ),
               ),
             ],
           ),